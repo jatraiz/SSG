ccf1a9d21063d9793fc45040db9e8301483883aa
John Stricker <john.stricker@raizlabs.com>
2014-02-21 11:12:16 -0500

    Rough command class set up

---
+++ SSGOGL/SSGOGL/SSGCommand.h
@@ -0,0 +1,13 @@
+//
+//  SSGCommand.h
+//  SSGOGL
+//
+//  Created by John Stricker on 2/21/14.
+//  Copyright (c) 2014 Sway Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSGCommand : NSObject
+
+@end
---
+++ SSGOGL/SSGOGL/SSGCommand.m
@@ -0,0 +1,13 @@
+//
+//  SSGCommand.m
+//  SSGOGL
+//
+//  Created by John Stricker on 2/21/14.
+//  Copyright (c) 2014 Sway Software. All rights reserved.
+//
+
+#import "SSGCommand.h"
+
+@implementation SSGCommand
+
+@end
