ecb3b313d2f1edd1916a271e0e8d2e7e95ff66f8
John Stricker <john.stricker@raizlabs.com>
2014-03-07 08:46:53 -0500

    added workspace for font testing

---
+++ SSGFonts/SSGFontTester/SSGFontTester/SSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSGAppDelegate.h
+//  SSGFontTester
+//
+//  Created by John Stricker on 3/7/14.
+//  Copyright (c) 2014 John Stricker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSGFonts/SSGFontTester/SSGFontTester/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSGFontTester
+//
+//  Created by John Stricker on 3/7/14.
+//  Copyright (c) 2014 John Stricker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSGAppDelegate class]));
+    }
+}
